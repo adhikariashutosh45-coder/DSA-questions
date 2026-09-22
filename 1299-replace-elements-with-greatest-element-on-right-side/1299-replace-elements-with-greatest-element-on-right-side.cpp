42e64828c3ca2ee1ec8ae3d036e978bc649de498
Ashutosh Adhikari <adhikariashutosh45@gmail.com>
2026-09-22 23:05:17 +0530

    Time: 0 ms (100%), Space: 72.6 MB (95.59%) - LeetHub

--- 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.cpp
+++ 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.cpp
@@ -2,14 +2,11 @@ class Solution {
 public:
     vector<int> replaceElements(vector<int>& nums) {
         int maxnum = -1;
-        for(int i=nums.size()-1;i>=0;i--) {
-            
-           int  currentnum=nums[i];
+        for (int i=nums.size()-1;i>=0;i--){
+            int currentnum =nums[i];
             nums[i]=maxnum;
-            maxnum=max(currentnum,maxnum);
-            
-            
-        }  
+            maxnum=max(maxnum,currentnum);
+        }
         return nums;
     }
 };
